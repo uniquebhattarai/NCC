3609ec29ab63f1a12aa29ed2210723fea40ed042
Unique <uniquebhattarai69@gmail.com>
2025-02-18 22:31:19 +0545

    Lab-12 updated

---
+++ File_12/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Text;
+namespace File_12;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Enter the text to write in the file: ");
+
+        string Path = @"/home/uniq/Desktop/UniqueNCCLAB/Unique.txt";
+        using (FileStream fs = new FileStream(Path,FileMode.Create,FileAccess.Write)){
+            using(StreamWriter writer = new StreamWriter(fs,Encoding.UTF8))
+            {
+                string input = Console.ReadLine();
+                writer.WriteLine(input);
+            }
+        }
+    }
+}
