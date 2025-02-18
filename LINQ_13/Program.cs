5eaa52592d823b655b501ebe9e1399664c5dcbe4
Unique <uniquebhattarai69@gmail.com>
2025-02-18 22:38:05 +0545

    Lab-13 LINQ

---
+++ LINQ_13/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LINQ_13;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        int[] age ={10,20,22,30,40,50,22,33,9,8,56,7};
+
+        var a = from i in age where i>20 orderby i select i;
+
+        foreach(int item in a){
+            System.Console.WriteLine(item);
+    }
+}
+}
