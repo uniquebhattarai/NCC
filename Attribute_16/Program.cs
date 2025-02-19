d1810fc31d15ef3981e953f4c6ed8a4dcb9d4c66
Unique <uniquebhattarai69@gmail.com>
2025-02-19 12:45:15 +0545

    Attribute

---
+++ Attribute_16/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Attribute_16;
+
+class Program
+{
+     [Obsolete("Don't use OldMethod, use NewMethod instead", false)]
+
+      static void OldMethod()
+    {
+        Console.WriteLine("It is the old method");
+    }
+
+    static void NewMethod()
+    {
+        Console.WriteLine("It is the new method");
+    }
+    static void Main(string[] args)
+    {
+        OldMethod();
+    }
+}
