ea3691469f4bcdd0c46ecb143e4b5692f73bfe7f
Unique <uniquebhattarai69@gmail.com>
2025-02-19 13:04:32 +0545

    event lab-9

---
+++ Event_9/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Event_9;
+
+// Publisher class that defines an event
+    class Publisher
+    {
+        public delegate void Notify(); // Declare delegate
+        public event Notify OnCompleted; // Declare event
+
+        public void Process()
+        {
+            Console.WriteLine("Processing...");
+
+            System.Threading.Thread.Sleep(2000);
+            Console.WriteLine("Process Completed!");
+
+            // Trigger event if there are subscribers
+            OnCompleted?.Invoke();
+        }
+    }
+
+    // Subscriber class that listens to the event
+    class Subscriber
+    {
+        public void EventHandler()
+        {
+            Console.WriteLine("Subscriber received the event notification.");
+        }
+    }
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Publisher pub = new Publisher(); // Create Publisher
+            Subscriber sub = new Subscriber(); // Create Subscriber
+
+            // Subscribe to the event
+            pub.OnCompleted += sub.EventHandler;
+
+            // Start process (will trigger event when completed)
+            pub.Process();
+    }
+}
